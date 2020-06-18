9a35908e96eb6fb3fcc1677379eb89619a32f81f
Miolin <miolin.dios@gmail.com>
2020-06-18 14:04:06 +0300

    change android share intent

--- android/src/main/java/com/example/fluttersharefile/FlutterShareFilePlugin.java
+++ android/src/main/java/com/example/fluttersharefile/FlutterShareFilePlugin.java
@@ -45,10 +45,7 @@ public class FlutterShareFilePlugin extends FlutterActivity implements MethodCal
     File imageFile = new File(instance.activeContext().getCacheDir(), fileName);
     Uri contentUri = FileProvider.getUriForFile(instance.activeContext(), "com.example.fluttersharefile", imageFile);
     Intent shareIntent = new Intent(Intent.ACTION_SEND);
-    shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri);
-    shareIntent.setType("image/png");
-    shareIntent.putExtra(Intent.EXTRA_TEXT, message);
-    shareIntent.setType("text/plain");
+    shareIntent.setDataAndType(contentUri, "image/png");
     instance.activity().startActivity(Intent.createChooser(shareIntent, "Share image using"));
 }
 }
